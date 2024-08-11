2518941f6dfe98778ab4bb584478a6063640551c
Himanshu <himanshu75439@gmail.com>
2024-08-11 23:34:46 +0530

    fix: Fixing puppeteer bugs

--- cron/lib/createAttachmentFromHTML.js
+++ cron/lib/createAttachmentFromHTML.js
@@ -1,5 +1,6 @@
 const PuppeteerHTMLPDF = require("puppeteer-html-pdf");
-const nodeHtmlToImage = require('node-html-to-image');
+// const nodeHtmlToImage = require('node-html-to-image');
+const puppeteer = require('puppeteer');
 
 module.exports = async (code, type='pdf') => {
     let path = null;
@@ -9,11 +10,19 @@ module.exports = async (code, type='pdf') => {
         if(type == 'img') {
             path = `${__dirname}/../file/${new Date().toISOString()}.png`;
             filename = 'invoice.png'
-            const options = {
-                output: path,
-                html: code
-            };
-            await nodeHtmlToImage(options);
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+              });
+            const page = await browser.newPage();
+            await page.setViewport({
+                height: 1200,
+                width: 800,
+                deviceScaleFactor: 1,
+            });            
+            await page.setContent(code);
+            await page.screenshot({path});
+            await browser.close();
         } else {
             path = `${__dirname}/../file/${new Date().toISOString()}.pdf`;
             filename = 'invoice.pdf';
