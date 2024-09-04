bfc02b18a8ded311f173485ff916f0c0fb44ca13
Himanshu <himanshu75439@gmail.com>
2024-09-04 17:29:24 +0530

    fix(cron): Removing html-pdf package

--- cron/lib/createAttachmentFromHTML.js
+++ cron/lib/createAttachmentFromHTML.js
@@ -1,10 +1,8 @@
 const PuppeteerHTMLPDF = require("puppeteer-html-pdf");
-// const nodeHtmlToImage = require('node-html-to-image');
 const puppeteer = require('puppeteer');
 
 // Keeping browser here to not open it for every operation
 let browser = null;
-let htmlPDF = null;
 
 let retry = 0;
 module.exports = async (code, type='pdf') => {
@@ -12,51 +10,50 @@ module.exports = async (code, type='pdf') => {
     let filename = null;
 
     try {
+
+        if(!browser) {
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+              });
+        }
+
+        const page = await browser.newPage();
+        await page.setViewport({
+            height: 1200,
+            width: 800,
+            deviceScaleFactor: 1,
+        });  
+        await page.setContent(code);
+
         if(type == 'img') {
             path = `${__dirname}/../file/${new Date().toISOString()}.png`;
             filename = 'invoice.png'
-
-            if(!browser) {
-                browser = await puppeteer.launch({
-                    headless: true,
-                    args: ['--no-sandbox', '--disable-setuid-sandbox']
-                  });
-            }
-            const page = await browser.newPage();
-            await page.setViewport({
-                height: 1200,
-                width: 800,
-                deviceScaleFactor: 1,
-            });            
-            await page.setContent(code);
             await page.screenshot({path});
-            await page.close();
-            // await browser.close();
-            retry = 0;
         } else {
             path = `${__dirname}/../file/${new Date().toISOString()}.pdf`;
             filename = 'invoice.pdf';
-            if(!htmlPDF) {
-                htmlPDF = new PuppeteerHTMLPDF();
-            }
-            const options = {
-                format: "A4",
-                path
-            };
-            htmlPDF.setOptions(options);
-            await htmlPDF.create(code);
-            await htmlPDF.closeBrowserTabs();
+            await page.pdf({
+                path,
+                format: 'A4',
+                printBackground: true
+            });
         }
+        
+        await page.close();
+        // await browser.close();
+        retry = 0;
+
     } catch (error) {
         console.log("Error in creating attachment: ", error);
-        if(type == 'img' && retry < 3) {
+        if(retry < 3) {
             retry++;
             await browser.close();
             browser = await puppeteer.launch({
                 headless: true,
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
               });
-            module.exports(code, type='img');
+            module.exports(code, type);
         }
     }
     
